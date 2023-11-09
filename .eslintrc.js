33de180ca2844e1610041cba3de0b54715e5095f
qiutian00 <1476102147@qq.com>
2023-11-09 18:02:08 +0800

    init commit

---
+++ .eslintrc.js
@@ -0,0 +1,27 @@
+/** @format */
+module.exports = {
+  parserOptions: {
+    ecmaVersion: 12,
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint/eslint-plugin'],
+  extends: ['plugin:@typescript-eslint/recommended', 'plugin:prettier/recommended'],
+  root: true,
+  env: {
+    node: true,
+    jest: true
+  },
+  ignorePatterns: ['dist', 'node_modules', '*.d.ts', 'examples/*.html'],
+  rules: {
+    semi: ['warn', 'never'],
+    eqeqeq: 'warn',
+    'no-else-return': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'comma-dangle': [2, 'never'],
+    'prettier/prettier': 'off',
+    'prefer-rest-params': 'off',
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/no-var-requires': 'off'
+  }
+}
