8b556dc412fc787a3da365326c66e2e35535fcc0
liwen03864 <liwen03864@sinobest.cn>
2023-11-10 11:44:04 +0800

    fix: change to class

--- src/core/userBehaviour.js
+++ src/core/userBehaviour.js
@@ -1,85 +1,78 @@
 import { bindHistoryEvent } from '../utils/event.ts'
 
-const userBehaviour = (function () {
-    const defaults = {
-        userInfo: true,
-        clicks: true,
-        mouseMovement: true,
-        mouseMovementInterval: 1,
-        mouseScroll: true,
-        mousePageChange: true,
-        timeCount: true,
-        clearAfterProcess: true,
-        processTime: 15,
-        processData: function (results) {
-            console.log(results);
-        }
-    };
+class userBehaviour {
+    constructor(defaults) {
+      this.defaults = defaults;
 
-    let user_config = {};
+      this.config(this.defaults);
 
-    const mem = {
-        processInterval: null,
-        mouseInterval: null,
-        mousePosition: [], //x,y,timestamp
-        eventListeners: {
-            scroll: null,
-            click: null,
-            mouseMovement: null,
-        },
-        eventsFunctions: {
-            scroll: () => {
-                results.mouseScroll.push([window.scrollX, window.scrollY, getTimeStamp()]);
+        this.mem = {
+            processInterval: null,
+            mouseInterval: null,
+            mousePosition: [], //x,y,timestamp
+            eventListeners: {
+                scroll: null,
+                click: null,
+                mouseMovement: null,
             },
-            click: (e) => {
-                results.clicks.clickCount++;
-                const path = [];
-                let node = "";
-                e.composedPath().forEach((el, i) => {
-                    if ((i !== e.composedPath().length - 1) && (i !== e.composedPath().length - 2)) {
-                        node = el.localName;
-                        (el.className !== "") ? el.classList.forEach((clE) => {
-                            node += "." + clE
-                        }): 0;
-                        (el.id !== "") ? node += "#" + el.id: 0;
-                        path.push(node);
-                    }
-                })
-                path = path.reverse().join(">");
-                results.clicks.clickDetails.push([e.clientX, e.clientY, path, getTimeStamp()]);
-            },
-            mouseMovement: (e) => {
-                mem.mousePosition = [e.clientX, e.clientY, getTimeStamp()];
-            },
-            pushState: (e) => {
-                const path = e && e.arguments.length > 2 && e.arguments[2];
-                const url = /^http/.test(path) ? path : (location.protocol + '//' + location.host + path);
-                console.log('pushState old:'+location.href,'new:'+url);
+            eventsFunctions: {
+                scroll: () => {
+                    this.results.mouseScroll.push([window.scrollX, window.scrollY, this.getTimeStamp()]);
+                },
+                click: (e) => {
+                    this.results.clicks.clickCount++;
+                    let path = [];
+                    let node = "";
+                    e.composedPath().forEach((el, i) => {
+                        if ((i !== e.composedPath().length - 1) && (i !== e.composedPath().length - 2)) {
+                            node = el.localName;
+                            (el.className !== "") ? el.classList.forEach((clE) => {
+                                node += "." + clE
+                            }): 0;
+                            (el.id !== "") ? node += "#" + el.id: 0;
+                            path.push(node);
+                        }
+                    })
+                    path = path.reverse().join(">");
+                    this.results.clicks.clickDetails.push([e.clientX, e.clientY, path, this.getTimeStamp()]);
+                },
+                mouseMovement: (e) => {
+                    this.mem.mousePosition = [e.clientX, e.clientY, this.getTimeStamp()];
+                },
+                pushState: (e) => {
+                    const path = e && e.arguments.length > 2 && e.arguments[2];
+                    const url = /^http/.test(path) ? path : (location.protocol + '//' + location.host + path);
+                    console.log('pushState old:'+location.href,'new:'+url);
 
-                results.mousePageChanges.push([{
-                    type: 'pushState',
-                    oldUrl: location.href,
-                    newUrl: url
-                }]);
-            },
-            replaceState: (e) => {
-                const path = e && e.arguments.length > 2 && e.arguments[2];
-                const url = /^http/.test(path) ? path : (location.protocol + '//' + location.host + path);
-                console.log('replaceState old:'+location.href,'new:'+url);
+                    this.results.mousePageChanges.push([{
+                        type: 'pushState',
+                        oldUrl: location.href,
+                        newUrl: url
+                    }]);
+                },
+                replaceState: (e) => {
+                    const path = e && e.arguments.length > 2 && e.arguments[2];
+                    const url = /^http/.test(path) ? path : (location.protocol + '//' + location.host + path);
+                    console.log('replaceState old:'+location.href,'new:'+url);
 
-                results.mousePageChanges.push([{
-                    type: 'replaceState',
-                    oldUrl: location.href,
-                    newUrl: url
-                }]);
+                    this.results.mousePageChanges.push([{
+                        type: 'replaceState',
+                        oldUrl: location.href,
+                        newUrl: url
+                    }]);
+                }
             }
-        }
-    };
+        };
 
-    let results = {};
+        this.results = {};
 
-    function resetResults() {
-        results = {
+        this.resetResults();
+    }
+
+    
+
+    resetResults() {
+        this.results = {
             userInfo: {
                 appCodeName: navigator.appCodeName || '',
                 appName: navigator.appName || '',
@@ -103,116 +96,133 @@ const userBehaviour = (function () {
         }
     };
 
-    resetResults();
+    
 
-    function getTimeStamp() {
+    getTimeStamp() {
         return Date.now();
     };
 
-    function config(ob) {
-        user_config = {};
-        Object.keys(defaults).forEach((i) => {
-            i in ob ? user_config[i] = ob[i] : user_config[i] = defaults[i];
+    config(ob) {
+        this.user_config = {};
+        Object.keys(this.defaults).forEach((i) => {
+            i in ob ? this.user_config[i] = ob[i] : this.user_config[i] = this.defaults[i];
         })
     };
 
-    function start() {
+    start() {
 
-        if (Object.keys(user_config).length !== Object.keys(defaults).length) {
+        if (Object.keys(this.user_config).length !== Object.keys(this.defaults).length) {
             console.log("no config provided. using default..");
-            user_config = defaults;
+            this.user_config = this.defaults;
         }
         // TIME SET
-        if (user_config.timeCount !== undefined && user_config.timeCount) {
-            results.time.startTime = getTimeStamp();
+        if (this.user_config.timeCount !== undefined && this.user_config.timeCount) {
+            this.results.time.startTime = this.getTimeStamp();
         }
         // MOUSE MOVEMENTS
-        if (user_config.mouseMovement) {
-            mem.eventListeners.mouseMovement = window.addEventListener("mousemove", mem.eventsFunctions.mouseMovement);
-            mem.mouseInterval = setInterval(() => {
-                if (mem.mousePosition && mem.mousePosition.length) { //if data has been captured
-                    if (!results.mouseMovements.length || ((mem.mousePosition[0] !== results.mouseMovements[results.mouseMovements.length - 1][0]) && (mem.mousePosition[1] !== results.mouseMovements[results.mouseMovements.length - 1][1]))) {
-                        results.mouseMovements.push(mem.mousePosition)
+        if (this.user_config.mouseMovement) {
+            this.mem.eventListeners.mouseMovement = window.addEventListener("mousemove", this.mem.eventsFunctions.mouseMovement);
+            this.mem.mouseInterval = setInterval(() => {
+                if (this.mem.mousePosition && this.mem.mousePosition.length) { //if data has been captured
+                    if (!this.results.mouseMovements.length || ((this.mem.mousePosition[0] !==  this.results.mouseMovements[this.results.mouseMovements.length - 1][0]) && (this.mem.mousePosition[1] !== this.results.mouseMovements[this.results.mouseMovements.length - 1][1]))) {
+                        this.results.mouseMovements.push(this.mem.mousePosition)
                     }
                 }
-            }, defaults.mouseMovementInterval * 1000);
+            }, this.defaults.mouseMovementInterval * 1000);
         }
         //CLICKS
-        if (user_config.clicks) {
-            mem.eventListeners.click = window.addEventListener("click", mem.eventsFunctions.click);
+        if (this.user_config.clicks) {
+            this.mem.eventListeners.click = window.addEventListener("click", this.mem.eventsFunctions.click);
         }
         //添加pushState replaceState event
-        if(user_config.mousePageChange) {
+        if(this.user_config.mousePageChange) {
             window.history['pushState'] = bindHistoryEvent('pushState')
             window.history['replaceState'] = bindHistoryEvent('replaceState')
         }
 
         // 添加监听事件
-        window.addEventListener('pushState', mem.eventsFunctions.pushState);
-        window.addEventListener('replaceState', men.eventsFunctions.replaceState);
+        window.addEventListener('pushState', this.mem.eventsFunctions.pushState);
+        window.addEventListener('replaceState', this.mem.eventsFunctions.replaceState);
         //SCROLL
-        if (user_config.mouseScroll) {
-            mem.eventListeners.scroll = window.addEventListener("scroll", mem.eventsFunctions.scroll);
+        if (this.user_config.mouseScroll) {
+            this.mem.eventListeners.scroll = window.addEventListener("scroll", this.mem.eventsFunctions.scroll);
         }
         //PROCESS INTERVAL
-        if (user_config.processTime !== false) {
-            mem.processInterval = setInterval(() => {
-                user_config.processData(result());
-            }, user_config.processTime * 1000)
+        if (this.user_config.processTime !== false) {
+            this.mem.processInterval = setInterval(() => {
+                this.user_config.processData(this.result());
+            }, this.user_config.processTime * 1000)
         }
     };
 
-    function processResults() {
-        user_config.processData(result());
-        if (user_config.clearAfterProcess) {
-            resetResults();
+    processResults() {
+        this.user_config.processData(this.result());
+        if (this.user_config.clearAfterProcess) {
+            this.resetResults();
         }
     }
 
-    function stop() {
+    stop() {
         if (user_config.processTime !== false) {
-            clearInterval(mem.processInterval);
+            clearInterval(this.mem.processInterval);
         }
-        clearInterval(mem.mouseInterval);
-        window.removeEventListener("scroll", mem.eventsFunctions.scroll);
-        window.removeEventListener("click", mem.eventsFunctions.click);
-        window.removeEventListener("mousemove", mem.eventsFunctions.mouseMovement);
+        clearInterval(this.mem.mouseInterval);
+        window.removeEventListener("scroll", this.mem.eventsFunctions.scroll);
+        window.removeEventListener("click", this.mem.eventsFunctions.click);
+        window.removeEventListener("mousemove", this.mem.eventsFunctions.mouseMovement);
     }
 
-    function result() {
-        if (user_config.userInfo === false && userBehaviour.showResult().userInfo !== undefined) {
+    result() {
+        if (this.user_config.userInfo === false && this.userBehaviour.showResult().userInfo !== undefined) {
             delete userBehaviour.showResult().userInfo;
         }
-        if (user_config.timeCount !== undefined && user_config.timeCount) {
-            results.time.currentTime = getTimeStamp();
+        if (this.user_config.timeCount !== undefined && this.user_config.timeCount) {
+            this.results.time.currentTime = this.getTimeStamp();
         }
-        return results
+        return this.results
     };
 
-    function showConfig() {
-        if (Object.keys(user_config).length !== Object.keys(defaults).length) {
-            return defaults;
+    showConfig() {
+        if (Object.keys(this.user_config).length !== Object.keys(this.defaults).length) {
+            return this.defaults;
         } else {
-            return user_config;
+            return this.user_config;
         }
     };
-    
-    return {
-        showConfig: showConfig,
-        config: config,
-        start: start,
-        stop: stop,
-        showResult: result,
-        processResults: processResults,
-    };
 
-})();
+};
+
+const defaults = {
+    userInfo: true,
+    clicks: true,
+    mouseMovement: true,
+    mouseMovementInterval: 1,
+    mouseScroll: true,
+    mousePageChange: true,
+    timeCount: true,
+    clearAfterProcess: true,
+    processTime: 15,
+    processData: function (results) {
+        console.log(results);
+    }
+};
+
+const instance =  new userBehaviour(defaults)
+
+const globalInstance =  {
+    showConfig: instance.showConfig.bind(instance),
+    config: instance.config.bind(instance),
+    start: instance.start.bind(instance),
+    stop: instance.stop.bind(instance),
+    showResult: instance.result.bind(instance),
+    processResults: instance.processResults.bind(instance),
+};
 
 // userBehaviour export to window
 if(window) {
-    window.userBehaviour = userBehaviour
+    window.userBehaviour = globalInstance
 }
 
 // console.log('userBehaviour exec', userBehaviour)
 
-export default userBehaviour
+export default globalInstance
