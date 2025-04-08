0f194aa0a4e136588a94a50563b21543551aa569
qiutian00 <1476102147@qq.com>
2025-04-08 21:38:24 +0800

    fix: 优化类型安全、事件监听防止内存泄漏，错误处理完善等

--- src/utils/event.ts
+++ src/utils/event.ts
@@ -1,16 +1,29 @@
 /**
- * 添加history原生并没有支持的pushState和replaceState的事件监听
- * @param type
- * @returns
+ * 为 History API 添加事件支持
+ * 
+ * 原生 History API 的 pushState 和 replaceState 方法不会触发事件，这个函数
+ * 创建了一个包装器，使这些方法在调用时能够触发自定义事件。
+ * 
+ * @param type - History API 方法名 ('pushState' 或 'replaceState')
+ * @returns 增强的 History 方法，执行原始操作并触发自定义事件
  */
 export const bindHistoryEvent = <T extends keyof History>(type: T): (() => any) => {
-  const historyEvent = history[type]
-  return function (this: any) {
-    // eslint-disable-next-line prefer-rest-params
-    const newEvent = historyEvent.apply(this, arguments) //执行history函数
-    const e = new Event(type) //声明自定义事件
-    ;(e as any).arguments = arguments
-    window.dispatchEvent(e) //抛出事件
-    return newEvent //返回方法，用于重写history的方法
-  }
-}
+  const originalMethod = history[type];
+  
+  return function (this: History, ...args: any[]) {
+    // 执行原始 History 方法
+    const result = originalMethod.apply(this, args);
+    
+    // 创建自定义事件
+    const event = new Event(type);
+    
+    // 将参数附加到事件对象
+    (event as any).arguments = args;
+    
+    // 触发事件
+    window.dispatchEvent(event);
+    
+    // 返回原始方法的结果
+    return result;
+  };
+};
